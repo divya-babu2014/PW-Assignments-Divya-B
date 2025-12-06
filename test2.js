a9863c36e3cbc01e2fa0ff9ca4da9b4cbbeb1e52
divya-babu2014 <divyababu.dv@gmail.com>
2025-12-06 17:22:44 +0530

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,23 @@
+let empname = "testing";   //  assignment
+console.log("empname:", empname);
+ 
+//  Re-assignment is allowed
+empname = "developer";
+console.log("empname after re-assignment:", empname);
+ 
+const age = "23";   //no declaration + assignment
+console.log("Age:", age);
+ 
+
+
+var emplname = "testing";   // declaration + assignment
+console.log("empname:", empname);
+ 
+//  Re-assignment is allowed
+emplname = "developer";
+console.log("empname after re-assignment:", emplname);
+//Re-dec is allowed
+var emplname = "sre";
+console.log("empname after re-assignment:", emplname);
+ 
+ 
